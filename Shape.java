6623829d59fc363c072d2d9034bdc01b2c5f9994
Pravallika-123-789 <98388936+Pravallika-123-789@users.noreply.github.com>
2022-02-09 16:38:39 +0530

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,18 @@
+package abstraction;
+
+public  abstract class Shape {
+	private String color;
+	abstract double area();
+	
+	public Shape(String color)
+	{
+		System.out.println("Construtor called");
+		this.color=color;
+	}
+	
+	public String getColor() {
+		return color;
+	}
+}
+
+
