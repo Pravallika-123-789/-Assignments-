ddd1976f58535787308fe6c3fd771b6958429032
Pravallika-123-789 <98388936+Pravallika-123-789@users.noreply.github.com>
2022-02-09 16:11:40 +0530

    Add files via upload

---
+++ AgeNotValidException.java
@@ -0,0 +1,19 @@
+package customexecption;
+
+public class AgeNotValidException extends Exception {
+
+private String msg;
+	
+	public AgeNotValidException(String msg) {
+		
+		this.msg=msg;
+	}
+
+	@Override
+	public String toString() {
+		return "AgeNotValidException [message=" + msg + "]";
+	}
+	
+	
+
+}
