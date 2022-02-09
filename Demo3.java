4496bec32fb1049e72d2b66bcc08fd6e2e343a21
Pravallika-123-789 <98388936+Pravallika-123-789@users.noreply.github.com>
2022-02-09 15:50:13 +0530

    Add files via upload

---
+++ Demo3.java
@@ -0,0 +1,12 @@
+package checkedexception;
+
+import java.sql.DriverManager;
+
+public class Demo3 {
+	
+		public static void main(String[] args) {
+			
+			DriverManager.getConnection("");
+		}
+
+}
