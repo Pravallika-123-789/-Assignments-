93549c7603ca434c66b0c45745eb16f6f86ee825
Pravallika-123-789 <98388936+Pravallika-123-789@users.noreply.github.com>
2022-02-09 16:10:11 +0530

    Add files via upload

--- Demo3.java
+++ Demo3.java
@@ -1,12 +1,12 @@
-package checkedexception;
-
-import java.sql.DriverManager;
+package uncheckedexception;
 
 public class Demo3 {
 	
-		public static void main(String[] args) {
-			
-			DriverManager.getConnection("");
-		}
+public static void main(String[] args) {
+		
+		int array[]= {5,6,34,8,9};
+		
+		System.out.println(array[5]);
+	}
 
 }
