c429b9f6ef3eff901b46c20125931821db6687ad
Pravallika-123-789 <98388936+Pravallika-123-789@users.noreply.github.com>
2022-02-09 15:49:48 +0530

    Add files via upload

---
+++ Demo2.java
@@ -0,0 +1,13 @@
+package checkedexception;
+
+import java.io.FileReader;
+
+public class Demo2 {
+	
+	public static void main(String[] args) {
+		
+		FileReader file= new FileReader("C://test//user.txt");
+		
+
+}
+}
