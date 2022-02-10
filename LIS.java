5d5d599b96327965baab3576be7979b70b5d7d41
Pravallika-123-789 <98388936+Pravallika-123-789@users.noreply.github.com>
2022-02-10 16:26:06 +0530

    Add files via upload

--- LIS.java
+++ LIS.java
@@ -5,7 +5,7 @@ import java.util.HashSet;
 public class LIS {
 	
 	public static void main(String[] args) {
-		int[] a= {3,9,1,10,4,20,2,11,12};
+		int[] a= {0,4,12,10,6,9,13,3,11,7,15};
 		HashSet<Integer>hs=new HashSet<>();
 		for(int i=0;i<a.length;i++) {
 			hs.add(a[i]);
